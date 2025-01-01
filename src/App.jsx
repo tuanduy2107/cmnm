561b3c3762cb279e95e9e5b596a42c765e6ce7df
ryuuduy <duy.nguyen21072003@hcmut.edu.vn>
2025-01-01 12:12:05 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css";
-import Login from './pages/Login';
+// import Login from './pages/Login';
 import GreetingForm from './pages/NewYear';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Login />}></Route>
-        <Route path='/new-year' element={<GreetingForm />}></Route>
+        <Route path='/' element={<GreetingForm />}></Route>
+        {/* <Route path='/new-year' element={<GreetingForm />}></Route> */}
       </Routes>
     </Router>
   );
